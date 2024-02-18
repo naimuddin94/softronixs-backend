ad3baa4c2c0c4112dfcaa50deb1571c8b0b32219
naimuddin94 <developernaimuddin@gmail.com>
2024-02-18 22:04:38 +0600

    server is running initially

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const globalErrorHandler = require("./lib/globalErrorHandler");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("softronixs is running....");
+});
+
+// handling all route which is not found
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl} on the server`);
+  error.status = 404;
+  next(error);
+});
+
+// error handling middleware
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ src/models/categorySchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// Define a sub-schema for the elements in the countryPriceList array
+const countryPriceSchema = mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  countryPriceList: [countryPriceSchema],
+});
+
+module.exports = categorySchema;
