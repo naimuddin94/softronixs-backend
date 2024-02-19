a1f93d7b7c6451071f899653d1166f384215e86c
naimuddin94 <developernaimuddin@gmail.com>
2024-02-20 03:41:55 +0600

    frontend url added into cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://softronixs.web.app",
+      "https://softronixs.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
