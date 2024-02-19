66550e57c257d91bc41639543cffc342ee9ffd31
naimuddin94 <developernaimuddin@gmail.com>
2024-02-20 03:28:03 +0600

    category update and delete implement

--- src/routeHandler/categoryHandler.js
+++ src/routeHandler/categoryHandler.js
@@ -12,6 +12,25 @@ const getAllCategories = async (req, res) => {
   }
 };
 
+// get single category by id
+const getCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+
+    if (!categoryId) {
+      return res.status(400).json({ message: "Category ID is required" });
+    }
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      return res.status(404).json({ message: "category not found" });
+    }
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 // create a new category
 const createCategory = async (req, res) => {
   console.log("log from 17", req.body);
@@ -23,7 +42,56 @@ const createCategory = async (req, res) => {
   }
 };
 
+// update a single category by id
+const updateCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+
+    if (!categoryId) {
+      return res.status(400).json({ message: "Category ID is required" });
+    }
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ message: "category not found" });
+    }
+
+    res.json({ message: "category updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// delete the category by id
+const deleteCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    if (!categoryId) {
+      return res.status(400).json({ message: "Category ID is required" });
+    }
+
+    const deletedCategory = await Category.findByIdAndDelete(categoryId);
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "category not found" });
+    }
+
+    res.json({ message: "category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllCategories,
   createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory,
 };
--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const {
   getAllCategories,
+  getCategory,
   createCategory,
+  updateCategory,
+  deleteCategory,
 } = require("../routeHandler/categoryHandler");
 
 const router = express.Router();
 
 router.get("/", getAllCategories);
+router.get("/:categoryId", getCategory);
 router.post("/create", createCategory);
+router.put("/update/:categoryId", updateCategory);
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
